24efd68249f758292717144a168cdadb52abd9c7
Brian Ibrahim Qamardeen <qeetell@protonmail.com>
2021-03-14 04:54:25 +0100

    2103-1404-54

--- main.go
+++ main.go
@@ -2,23 +2,52 @@ package main
 
 import (
 	"fmt"
-	"time"
-
 	"github.com/inancgumus/screen"
 )
 
 func main () {
-	// Clear all the characters on the screen
-	screen.Clear ()
-
+	c := make (chan [2]string)
+	go taskThread (c)
+	outputList := []string {"Code ID: 2103-0502-33 --- Code Name: Supreme potato"}
+	
 	for {
+		message := <- c
+		c <- [2]string {"oo", "ok"}
+		
+		if message [0] == "l2" {
+			newOutputList := []string {outputList [0]}
+			newOutputList = append (newOutputList, message [1])
+			outputList = newOutputList
+		}
+		if message [0] == "l3" {
+			newOutputList := []string {outputList [0], outputList [1]}
+			newOutputList = append (newOutputList, message [1])
+			outputList = newOutputList
+		}
+		if message [0] == "l4" {
+			newOutputList := []string {outputList [0], outputList [1],
+				outputList [2]}
+			newOutputList = append (newOutputList, message [1])
+			outputList = newOutputList
+		}
+		if message [0] == "hl" {
+			break
+		}
+		
 		screen.Clear ()
-		// Moves the cursor to the top-left position of the screen
-		screen.MoveTopLeft ()
 
-		// Animate the time always in the same position
-		fmt.Println (time.Now ())
+		fmt.Println (outputList [0] + "\n")
 
-		time.Sleep (time.Second)
+		if len (outputList) == 2 || len (outputList) > 2 {
+			fmt.Println (outputList [1] + "\n\n++++  ++++  ++++  ++++\n")
+		}
+
+		if len (outputList) == 3 || len (outputList) > 3 {
+			fmt.Println (outputList [2] + "\n")
+		}
+
+		if len (outputList) == 4 || len (outputList) > 4 {
+			fmt.Println (outputList [3] + "\n")
+		}
 	}
 }
